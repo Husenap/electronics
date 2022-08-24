c36f756b1ddecd7148cb2a4557aa89cd23f8e4aa
Hussein Taher <6496177+Husenap@users.noreply.github.com>
2022-08-24 20:10:30 +0200

    updated controller definition

--- ir-sensor/ir-sensor/controller-definitions.hpp
+++ ir-sensor/ir-sensor/controller-definitions.hpp
@@ -2,7 +2,7 @@
 
 #include "controller.hpp"
 
-// Small controller for LED strips
+// Andersson LSD 3.0 IR LED Controller
 static constexpr Controller Andersson{
     .name = "Andersson",
     .bits = 32,
@@ -16,4 +16,30 @@ static constexpr Controller Andersson{
     .repeat = {9500, 2126},
 
     .gap = 108500,
+};
+enum class AnderssonButtons {
+  Power       = 0xffe01f,
+  Auto        = 0xffd02f,
+  Randomize   = 0xfff00f,
+  Flash       = 0xffe817,
+  Jump        = 0xffd827,
+  Meteor      = 0xffc837,
+  Www         = 0xff609f,
+  Inwards     = 0xff50af,
+  Outwards    = 0xff10ef,
+  Up          = 0xffa05f,
+  Down        = 0xff20df,
+  ICSet       = 0xff906f,
+  CS          = 0xffb04f,
+  C3          = 0xffa857,
+  C7          = 0xff9867,
+  C16         = 0xff8877,
+  Faster      = 0xff708f,
+  Slower      = 0xff30cf,
+  Brighter    = 0xff6897,
+  Dimmer      = 0xff28d7,
+  Right       = 0xff58a7,
+  Left        = 0xff18e7,
+  MeteorLeft  = 0xff08f7,
+  MeteorRight = 0xff48b7,
 };

8c4baef5becc57f7c91e612373c7a2b83b2cddf2
Hussein Taher <6496177+Husenap@users.noreply.github.com>
2022-08-24 21:15:48 +0200

    added livarno lux definition

--- ir-sensor/ir-sensor/controller-definitions.hpp
+++ ir-sensor/ir-sensor/controller-definitions.hpp
@@ -43,3 +43,45 @@ enum class AnderssonButtons {
   MeteorLeft  = 0xff08f7,
   MeteorRight = 0xff48b7,
 };
+
+// LivarnoLux IR LED Controller
+static constexpr Controller LivarnoLux{
+    .name = "Livarno Lux",
+    .bits = 32,
+    .eps  = 30,
+    .aeps = 100,
+
+    .header = {9500, 4400},
+    .one    = {674, 1500},
+    .zero   = {674, 440},
+
+    .repeat = {9500, 2126},
+
+    .gap = 108500,
+};
+enum class LivarnoLuxButtons {
+  On       = 0xffb04f,
+  Off      = 0xfff807,
+  Brighter = 0xff906f,
+  Dimmer   = 0xffb847,
+  Flash    = 0xffb24d,
+  Strobe   = 0xff00ff,
+  Fade     = 0xff58a7,
+  Smooth   = 0xff30cf,
+  White    = 0xffa857,
+  Red      = 0xff9867,
+  Red1     = 0xffe817,
+  Red2     = 0xff02fd,
+  Red3     = 0xff50af,
+  Red4     = 0xff38c7,
+  Green    = 0xffd827,
+  Green1   = 0xff48b7,
+  Green2   = 0xff32cd,
+  Green3   = 0xff7887,
+  Green4   = 0xff28d7,
+  Blue     = 0xff8877,
+  Blue1    = 0xff6897,
+  Blue2    = 0xff20df,
+  Blue3    = 0xff708f,
+  Blue4    = 0xfff00f,
+};
